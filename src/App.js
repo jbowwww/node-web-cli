2a538a8d8aec8e6213e988e4ae48b0110b76bfba
Joel <jbowwww@gmail.com>
2018-04-21 00:23:34 +0800

    Minor refactoring/clean up src/App.js

--- src/App.js
+++ src/App.js
@@ -2,55 +2,58 @@ import React from 'react';
 import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import Console from './Console.js'
 import makeRef from './makeref.js';
+import classnames from 'classnames';
 import logo from './logo.svg';
 import './App.css';
 
-import innerClassnames from 'classnames';
-const classnames = (...args) => innerClassnames(...args) + ' nav-tab';
 
 export default class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.console = makeRef();
-    this.toggle = this.toggle.bind(this);
+	constructor(props) {
+	    super(props);
+	    this.console = makeRef();
+	    this.toggle = this.toggle.bind(this);
 		this.ws = new WebSocket(this.props.cmdServer);//'ws://127.0.0.1:8080');
 		this.ws.onmessage = msg => this.console.current.appendOutput(JSON.parse(msg.data));
-		this.state = {
-      activeTab: '1'
-    };
-  }
+		this.state = { activeTab: '1' };
+	}
 
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({ activeTab: tab });
-    }
-  }
+	toggle(tab) {
+	    if (this.state.activeTab !== tab) {
+	    	this.setState({ activeTab: tab });
+	    }
+	}
 
-  componentDidMount() { }
+	activeTabClassName(tab) {
+		var activeTab = this.state.activeTab;
+		// using == should perform type conversion between ints and strings representing ints (unlike ===)
+		return classnames({ active: activeTab === tab || activeTab === tab.toString() }) + ' nav-tab';
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <Nav tabs>
+	componentDidMount() { }
+
+	render() {
+    	return (
+			<div className="App">
+	        	<Nav tabs>
 					<div className="App-cmd-server"><div>cmdServer: {this.props.cmdServer}</div></div>
-          <NavItem>
-            <NavLink className={classnames({ active: this.state.activeTab === '1' })} onClick={() => this.toggle('1')}>Console</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className={classnames({ active: this.state.activeTab === '2' })} onClick={() => this.toggle('2')}>Diagnostics</NavLink>
-          </NavItem>
+		          	<NavItem>
+						<NavLink className={this.activeTabClassName(1)} onClick={() => this.toggle('1')}>Console</NavLink>
+		        	</NavItem>
+		        	<NavItem>
+		        		<NavLink className={this.activeTabClassName(2)} onClick={() => this.toggle('2')}>Diagnostics</NavLink>
+		    		</NavItem>
 					<img src={logo} className="App-logo" alt="logo" />
-        </Nav>
-      	<TabContent activeTab={this.state.activeTab}>
-        	<TabPane tabId="1">
-            <Console ref={this.console} ws={this.ws} />
-          </TabPane>
-          <TabPane tabId="2">
-
+		        </Nav>
+	      		<TabContent activeTab={this.state.activeTab}>
+		        	<TabPane tabId="1">
+		            	<Console ref={this.console} ws={this.ws} />
+		        	</TabPane>
+	          		<TabPane tabId="2">
+						&nbsp;
 					</TabPane>
-    		</TabContent>
-    	</div>
-  	);
+	    		</TabContent>
+    		</div>
+		);
 	}
 }
